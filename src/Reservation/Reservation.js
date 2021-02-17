f2b4657d0ae2ba5006c43729949f9b870fc2c6c3
conconartist <conconart.info@gmail.com>
2021-02-17 11:22:13 -0700

    Add to Reservation display

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -4,9 +4,9 @@ import './Reservation.css';
 const Reservation = ({id, resName, date, time, number}) => {
     return (
         <div className='res-card'>
-            <h2 id={id}>{resName}</h2>
+            <h2 id={id} className='res-name'>{resName}</h2>
             <p className='date'>{date}</p>
-            <p className='time'>{time.toLocaleString('en-US')}</p>
+            <p className='time'>{time} pm</p>
             <p className='number'>Number of guests: {number}</p>
             <button className="cancel-button">Cancel</button>
         </div>
