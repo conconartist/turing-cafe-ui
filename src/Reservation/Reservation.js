d68b9b2f8d510cad731636de8854eaf4db3e01b9
conconartist <conconart.info@gmail.com>
2021-02-17 10:35:04 -0700

    Update Reservation functionality

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Reservation.css';
 
-const Reservation = ({id, resName, time, number}) => {
+const Reservation = ({id, resName, date, time, number}) => {
     return (
         <div className='res-card'>
             <h2 id={id}>{resName}</h2>
+            <p className='date'>{date}</p>
+            <p className='time'>{time.toLocaleString('en-US')}</p>
+            <p className='number'>Number of guests: {number}</p>
+            <button className="cancel-button">Cancel</button>
         </div>
     )
 }
